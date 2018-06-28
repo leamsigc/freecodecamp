543050f326596c40de9800286ccf27e817a56922
leamsigc <leamsigc@hotmail.com>
2018-06-28 15:29:03 -0400

    finish the last project and get the certification on frecodecamp after 4 projects

--- TechnicalDocumentation /main.js
+++ TechnicalDocumentation /main.js
@@ -1,1 +1,15 @@
-console.log('Welcome to the app');
+window.addEventListener('DOMContentLoaded', ()=>{
+    console.log('hello from the console');
+    const $nav = document.querySelector('ul[data-js="nav-bar"]');
+     $nav.addEventListener('click', scrollToElement);
+});
+
+function scrollToElement(e) {
+    e.preventDefault();
+    const elementToScrollTo = document.querySelector(`${e.target.getAttribute('href')}`);
+    elementToScrollTo.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest'
+    });
+}
